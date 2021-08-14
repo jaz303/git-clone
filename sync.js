e1e78c91625ad5d3e053ea39ed34eded0c4ad865
Jason Frame <jwf@jasonframe.co.uk>
2021-08-14 23:02:54 +0100

    remove sync stub, I'll get to it later

--- sync.js
+++
@@ -1,8 +0,0 @@
-const spawnSync = require('child_process').spawnSync;
-const makePlan = require('./private/planner');
-
-module.exports = function gitCloneSync(repo, targetPath, opts) {
-    const plan = makePlan(repo, targetPath, opts);
-
-    // TODO: synchronous ops
-}
