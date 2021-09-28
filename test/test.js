078e9a66ae3db2d8b802a0eb9703b20cdab19ad4
Jason Frame <jwf@jasonframe.co.uk>
2021-09-28 10:38:20 +0100

    Update tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,59 @@
-var gitClone = require('../index');
+const fs = require('fs');
+const {execSync} = require('child_process');
 
-gitClone('git@github.com:jaz303/git-clone.git', './test-checkout', (err) => {
-	console.log("complete!");
-	console.log(err);
-});
+const gitClone = {
+	callback: require('../'),
+	promise: require('../promise')
+};
+
+function assertGitRepoSync(dir, revision) {
+	const stats = fs.statSync(`${dir}/.git`);
+	return stats.isDirectory();
+}
+
+function assertCheckout(dir, expectedCheckout) {
+	const checkedOut = execSync('git rev-parse HEAD', {cwd: dir, encoding: 'utf8'}).trim();
+	return checkedOut === expectedCheckout;
+}
+
+let nextCheckoutId = 1;
+function runTestCase(name, opts, fn) {
+	const id = nextCheckoutId++;
+	const targetDir = `./test-checkout-${Date.now()}-${id}`;
+
+	fn(targetDir, opts, (err) => {
+		if (err) {
+			console.error(`Test '${name}' failed: ${err.message}`);
+		} else if (!assertGitRepoSync(targetDir)) {
+			console.error(`Test '${name}' failed: target directory is not a git repository`);
+		} else if (opts && opts.checkout && !assertCheckout(targetDir, opts.checkout)) {
+			console.error(`Test '${name}' failed: incorrect checkout`);
+		} else {
+			console.error(`Test '${name}': OK`);
+		}
+		execSync(`rm -rf ${targetDir}`);
+	});
+}
+
+const callbackTest = (targetDir, options, onComplete) => {
+	if (options === null) {
+		gitClone.callback('git@github.com:jaz303/git-clone.git', targetDir, onComplete);
+	} else {
+		gitClone.callback('git@github.com:jaz303/git-clone.git', targetDir, options, onComplete);
+	}
+};
+
+const promiseTest = async (targetDir, options, onComplete) => {
+	try {
+		await gitClone.promise('git@github.com:jaz303/git-clone.git', targetDir, options);
+		onComplete(null);
+	} catch (err) {
+		onComplete(err);
+	}
+}
+
+runTestCase('callback', {}, callbackTest);
+runTestCase('callback (null options)', null, callbackTest); // tests argument juggling
+runTestCase('callback with checkout', {checkout: 'ddb88d4d1dca74e8330eccb7997badd6a6906b98'}, callbackTest);
+runTestCase('promise', {}, promiseTest);
+runTestCase('promise with checkout', {checkout: 'ddb88d4d1dca74e8330eccb7997badd6a6906b98'}, promiseTest);
